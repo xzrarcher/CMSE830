976eaee744a0d8770b06be2066e47537b98362d1
xzrarcher <43585298+xzrarcher@users.noreply.github.com>
2023-12-05 22:25:11 -0500

    Add files via upload

---
+++ 2/model_foefficients.py
@@ -0,0 +1,17 @@
+# Coefficients for the housing price prediction model
+coefficients = {
+    "ocean_proximity_ISLAND": 130113.5963943309,
+    "median_income": 39259.572876434,
+    "housing_median_age": 1072.5200416374466,
+    "total_bedrooms": 100.5562902566644,
+    "households": 49.61732607337626,
+    "total_rooms": -6.193263720546383,
+    "population": -37.96908288134364,
+    "ocean_proximity_NEAR OCEAN": -18510.21040035283,
+    "ocean_proximity_<1H OCEAN": -22788.344724717244,
+    "latitude": -25482.18483097187,
+    "ocean_proximity_NEAR BAY": -26742.39634957774,
+    "longitude": -26812.989281730384,
+    "ocean_proximity_INLAND": -62072.64491968309,
+    "intercept": -2247165.771408345
+}
